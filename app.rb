3d43558cf2310ae60057885f426d078c71410e19
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-02 18:04:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
+  get '/' do
     erb :index
   end
+
+  get '/create_puppy' do
+    erb :create_puppy
+  end 
 
 
 
--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,10 @@
 class Puppy
-attr_accessor :name, :breed, :age 
+attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
     @name = name
-    @breed = breed 
+    @breed = breed
     @age = age 
-  end 
-  
-end 
+  end
+
+end
